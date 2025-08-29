d86ad18d94e92dfba5e3ca084c52920ee6be5b91
Sean Kirubi <seannganga17@gmail.com>
2025-08-29 12:29:53 +0300

    Move logos and icons into src/assests/images

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,5 +1,7 @@
 import { Header } from "../components/Header";
 import { Link } from "react-router";
+import BuyAgainIcon from "../assets/images/icons/buy-again.png"
+
 import "./OrdersPage.css";
 
 export function OrdersPage() {
@@ -49,7 +51,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainIcon}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -78,7 +80,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainIcon}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -127,7 +129,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainIcon}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
