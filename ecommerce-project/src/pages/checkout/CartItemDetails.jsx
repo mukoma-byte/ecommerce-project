ffe24109c5fd078c26033c5b8148df06323a3b21
Sean Kirubi <seannganga17@gmail.com>
2025-09-05 07:28:01 +0300

    In cartitemdetails make update show a text  box on click

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,6 +1,15 @@
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 import axios from "axios";
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [updateQuantity, setUpdateQuantity] = useState(false)
+  const updateQuantityState = () => {
+    if(updateQuantity){
+      setUpdateQuantity(false)
+    }else{
+      setUpdateQuantity(true)
+    }
+  }
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
@@ -16,9 +25,19 @@ export function CartItemDetails({ cartItem, loadCart }) {
         <div className="product-quantity">
           <span>
             Quantity:
+            {updateQuantity ? (
+              <input type="text" className="quantity-input" />
+            ) : (
+              ""
+            )}
             <span className="quantity-label">{` ${cartItem.quantity}`}</span>
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={updateQuantityState}
+          >
+            Update
+          </span>
           <span
             className="delete-quantity-link link-primary"
             onClick={deleteCartItem}
