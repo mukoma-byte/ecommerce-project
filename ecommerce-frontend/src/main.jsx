b9d0be8bbe9af2dbf387b6b1a32798760fb9b86c
Sean Kirubi <seannganga17@gmail.com>
2025-10-14 11:20:03 +0300

    modify main.jsx to include AuthProvider.jsx

--- ecommerce-frontend/src/main.jsx
+++ ecommerce-frontend/src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx"; // ✅ import AuthProvider
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    {/* ✅ Wrap the whole app inside AuthProvider */}
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>
+);
