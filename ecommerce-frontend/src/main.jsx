93b439be527d0f2959bf40c29b747997fb080612
Sean Kirubi <seannganga17@gmail.com>
2025-10-30 14:50:41 +0300

    Configure axios in main.jsx

--- ecommerce-frontend/src/main.jsx
+++ ecommerce-frontend/src/main.jsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router'
-import './index.css'
-import App from './App.jsx'
+import axios from "axios";
 
-createRoot(document.getElementById('root')).render(
+axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.withCredentials = true;
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
