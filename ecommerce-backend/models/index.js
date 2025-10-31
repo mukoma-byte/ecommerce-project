028aa4c86aa4d81ae8d8926fdb706c111b26af49
SeanTheDev <seankirubi0@gmail.com>
2025-10-31 15:29:59 +0300

    replace index.js code

--- ecommerce-backend/models/index.js
+++ ecommerce-backend/models/index.js
@@ -1,9 +1,11 @@
-import { Sequelize } from 'sequelize';
-import sqlJsAsSqlite3 from 'sql.js-as-sqlite3';
-import fs from 'fs';
+import { Sequelize } from "sequelize";
 
-const isUsingRDS = process.env.RDS_HOSTNAME && process.env.RDS_USERNAME && process.env.RDS_PASSWORD;
-const dbType = process.env.DB_TYPE || 'mysql';
+const isUsingRDS =
+  process.env.RDS_HOSTNAME &&
+  process.env.RDS_USERNAME &&
+  process.env.RDS_PASSWORD;
+
+const dbType = process.env.DB_TYPE || "mysql";
 const defaultPorts = {
   mysql: 3306,
   postgres: 5432,
@@ -20,29 +22,12 @@ if (isUsingRDS) {
     host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT || defaultPort,
     dialect: dbType,
-    logging: false
+    logging: false,
   });
 } else {
   sequelize = new Sequelize({
-    dialect: 'sqlite',
-    dialectModule: sqlJsAsSqlite3,
-    logging: false
+    dialect: "sqlite",
+    storage: "./database.sqlite",
+    logging: false,
   });
-
-  // Save database to file after write operations.
-  sequelize.addHook('afterCreate', saveDatabaseToFile);
-  sequelize.addHook('afterDestroy', saveDatabaseToFile);
-  sequelize.addHook('afterUpdate', saveDatabaseToFile);
-  sequelize.addHook('afterSave', saveDatabaseToFile);
-  sequelize.addHook('afterUpsert', saveDatabaseToFile);
-  sequelize.addHook('afterBulkCreate', saveDatabaseToFile);
-  sequelize.addHook('afterBulkDestroy', saveDatabaseToFile);
-  sequelize.addHook('afterBulkUpdate', saveDatabaseToFile);
-}
-
-export async function saveDatabaseToFile() {
-  const dbInstance = await sequelize.connectionManager.getConnection();
-  const binaryArray = dbInstance.database.export();
-  const buffer = Buffer.from(binaryArray);
-  fs.writeFileSync('database.sqlite', buffer);
 }
